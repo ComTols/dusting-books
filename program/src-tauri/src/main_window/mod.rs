c510305d3f24c220530fec547fbad13c8fc785dc
ComTols <maximilian.mg.schueller@gmail.com>
2025-06-01 19:05:24 +0200

    add: main window static and public accessible

--- program/src-tauri/src/main_window/mod.rs
+++ program/src-tauri/src/main_window/mod.rs
@@ -1,31 +1,46 @@
-use tauri::{AppHandle, Manager, WebviewUrl, WebviewWindowBuilder, WindowEvent};
+use std::sync::{LazyLock, Mutex};
+use tauri::{AppHandle, Manager, WebviewWindow, WebviewWindowBuilder, WindowEvent};
+use tauri::utils::config::WindowConfig;
 
-const MAIN_WINDOW: &str = "main_window";
+const MAIN_WINDOW_LABEL: &str = "main_window";
+pub static MAIN_WINDOW: LazyLock<Mutex<MainWindow>> =
+    LazyLock::new(|| Mutex::new(MainWindow::default()));
 
-pub struct MainWindow {}
+pub struct MainWindow {
+    window_config: WindowConfig,
+    window: Option<WebviewWindow>,
+}
 
 impl MainWindow {
-    pub fn open(&self, app: &AppHandle) -> tauri::Result<()> {
-        let main_window = app.get_webview_window(MAIN_WINDOW);
-
-        if main_window.is_none() {
-            return self.build(app);
+    pub fn open(&mut self, app: &AppHandle) -> tauri::Result<()> {
+        if self.window.is_none() {
+            let main_window = app.get_webview_window(MAIN_WINDOW_LABEL);
+            self.window = match main_window {
+                Some(m_window) => Some(m_window),
+                None => Some(self.build(app)?),
+            }
         }
 
-        let _main_window = main_window.unwrap();
+        if let Some(window) = &self.window {
+            window.show()?;
+        }
 
         Ok(())
     }
 
-    fn build(&self, app: &AppHandle) -> tauri::Result<()> {
-        let window = WebviewWindowBuilder::new(app, MAIN_WINDOW, WebviewUrl::default()).build()?;
-        window.on_window_event(|event| MainWindow::on_window_event(event));
-        Ok(())
+    fn build(&self, app: &AppHandle) -> tauri::Result<WebviewWindow> {
+        let window = WebviewWindowBuilder::from_config(app, &self.window_config)?.build()?;
+        let cloned_window = window.clone();
+        window.on_window_event(move |event| MainWindow::on_window_event(event, &cloned_window));
+        Ok(window)
     }
 
-    fn on_window_event(event: &WindowEvent) {
+    fn on_window_event(event: &WindowEvent, window: &WebviewWindow) {
         match event {
-            WindowEvent::CloseRequested { api, .. } => api.prevent_close(),
+            WindowEvent::CloseRequested { api, .. } => {
+                api.prevent_close();
+                window.hide().unwrap();
+            }
             _ => {}
         }
     }
@@ -33,6 +48,16 @@ impl MainWindow {
 
 impl Default for MainWindow {
     fn default() -> Self {
-        Self {}
+        Self {
+            window_config: WindowConfig {
+                create: false,
+                width: 800.0,
+                height: 600.0,
+                maximized: true,
+                label: MAIN_WINDOW_LABEL.to_string(),
+                ..WindowConfig::default()
+            },
+            window: None
+        }
     }
 }
--- program/src-tauri/src/tray_service/mod.rs
+++ program/src-tauri/src/tray_service/mod.rs
@@ -1,17 +1,16 @@
+use crate::main_window::MAIN_WINDOW;
 use image::GenericImageView;
-use tauri::tray::{MouseButton, TrayIconEvent};
-use tauri::tray::TrayIcon;
-use tauri::tray::TrayIconBuilder;
-use tauri::{App, AppHandle, Wry};
 use tauri::image::Image;
 use tauri::menu::{Menu, MenuEvent, MenuItem};
-use crate::main_window::MainWindow;
+use tauri::tray::TrayIcon;
+use tauri::tray::TrayIconBuilder;
+use tauri::tray::{MouseButton, TrayIconEvent};
+use tauri::{App, AppHandle, Wry};
 
 const TRAY_MENU_ID: &str = "tray_menu";
 const TRAY_MENU_QUIT_ID: &str = "tray_menu_quit";
 
-pub struct TrayService {
-}
+pub struct TrayService {}
 
 impl TrayService {
     pub fn build(&self, app: &mut App) -> tauri::Result<()> {
@@ -32,7 +31,8 @@ impl TrayService {
 
     fn load_icon() -> tauri::Result<Image<'static>> {
         let image_bytes = include_bytes!("../../icons/tray-icon.png");
-        let image = image::load_from_memory(image_bytes).map_err(|e| tauri::Error::AssetNotFound(e.to_string()))?;
+        let image = image::load_from_memory(image_bytes)
+            .map_err(|e| tauri::Error::AssetNotFound(e.to_string()))?;
         let (width, height) = image.dimensions();
         let rgba_binary = image.to_rgba8().into_raw();
 
@@ -42,9 +42,7 @@ impl TrayService {
     fn menu(&self, app: &mut App) -> tauri::Result<Menu<Wry>> {
         let quit_item = MenuItem::with_id(app, TRAY_MENU_QUIT_ID, "Beenden", true, None::<&str>)?;
 
-        Menu::with_id_and_items(app, TRAY_MENU_ID, &[
-            &quit_item,
-        ])
+        Menu::with_id_and_items(app, TRAY_MENU_ID, &[&quit_item])
     }
 
     fn on_tray_icon_event(app: &TrayIcon, event: TrayIconEvent) {
@@ -53,7 +51,8 @@ impl TrayService {
                 button: MouseButton::Left,
                 ..
             } => {
-                MainWindow::default().open(app.app_handle()).unwrap()
+                let mut main_window = MAIN_WINDOW.lock().unwrap();
+                main_window.open(app.app_handle()).unwrap();
             }
             _ => {}
         }
@@ -70,8 +69,6 @@ impl TrayService {
 
 impl Default for TrayService {
     fn default() -> Self {
-        TrayService {
-
-        }
+        TrayService {}
     }
 }
